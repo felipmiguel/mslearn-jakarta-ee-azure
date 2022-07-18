623d35b3380b3c6ea9002a28f6b69e5fcd7a068a
Felip Miguel <fmiguel@microsoft.com>
2022-07-18 11:01:56 +0200

    working now

--- src/main/webapp/WEB-INF/createMySQLDataSource.sh
+++ src/main/webapp/WEB-INF/createMySQLDataSource.sh
@@ -8,7 +8,6 @@ data-source add --name=JPAWorldDataSourceDS \
 --connection-url=${MYSQL_CONNECTION_URL} \
 --driver-name=ROOT.war_com.mysql.cj.jdbc.Driver_8_0 \
 --user-name=${MYSQL_USER} \
---password=${MYSQL_PASSWORD} \
 --min-pool-size=5 \
 --max-pool-size=20 \
 --blocking-timeout-wait-millis=5000 \
@@ -16,7 +15,8 @@ data-source add --name=JPAWorldDataSourceDS \
 --driver-class=com.mysql.cj.jdbc.Driver \
 --jta=true \
 --use-java-context=true \
---valid-connection-checker-class-name=org.jboss.jca.adapters.jdbc.extensions.mysql.MySQLValidConnectionChecker \
+--validate-on-match=false \
+--background-validation=false \
 --exception-sorter-class-name=com.mysql.cj.jdbc.integration.jboss.ExtendedMysqlExceptionSorter
 exit
 EOF
